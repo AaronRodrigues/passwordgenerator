7795ef8b3822293f1cce8c5e8ad6623fa98167d0
AaronRodrigues <aaronrod@gmail.com>
2017-09-30 18:38:02 +0100

    Password is generated with lowercase letters

--- lib/passwordgenerator.rb
+++ lib/passwordgenerator.rb
@@ -2,10 +2,8 @@ class PasswordGenerator
     
   def makeit(length = 15)
     secret = []
-    character_array = ('a'..'z').to_a
-    num_array = (0..9).to_a
-    secret.concat(character_array.shuffle)
-    secret.concat(num_array.shuffle)
+    lowercase_array = ('a'..'z').to_a
+    secret.concat(lowercase_array.shuffle)
     password = secret.shuffle.take(length).join
   end
 end
