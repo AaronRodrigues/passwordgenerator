42ee8613255bf30b00514bcec85ef9c4549b616d
AaronRodrigues <aaronrod@gmail.com>
2017-09-30 23:14:19 +0100

    Adds guard condition for lowercase concatenation

--- lib/passwordgenerator.rb
+++ lib/passwordgenerator.rb
@@ -1,9 +1,9 @@
 class PasswordGenerator
     
-  def makeit(length = 15, uppercase = true, numeric = true, special = true)
+  def makeit(length = 15, uppercase = true, lowercase = true, numeric = true, special = true)
     secret = []
-    secret.concat(create_lowercase_array.shuffle)
     secret.concat(create_uppercase_array.shuffle) if uppercase == true
+    secret.concat(create_lowercase_array.shuffle) if lowercase == true
     secret.concat(create_num_array.shuffle) if numeric == true
     secret.concat(create_special_array.shuffle) if special == true
     password = secret.shuffle.take(length).join
