4a8f59f2fa4b6354c87c5611b4049e759b68283e
AaronRodrigues <aaronrod@gmail.com>
2017-09-30 18:33:44 +0100

    Sets the default password length as 15

--- lib/passwordgenerator.rb
+++ lib/passwordgenerator.rb
@@ -1,3 +1,11 @@
 class PasswordGenerator
-  
+    
+  def makeit(length = 15)
+    secret = []
+    character_array = ('a'..'z').to_a
+    num_array = (0..9).to_a
+    secret.concat(character_array.shuffle)
+    secret.concat(num_array.shuffle)
+    password = secret.shuffle.take(length).join
+  end
 end
--- spec/password_spec.rb
+++ spec/password_spec.rb
@@ -6,4 +6,23 @@ describe PasswordGenerator do
   it 'should exist' do
     expect(passwordgenerator).to be_truthy
   end
+  
+  it 'should generate lowercase letters' do
+    allow(passwordgenerator).to receive(:makeit).and_return('abcdefghijkl')
+    expect(passwordgenerator.makeit).to eq ('abcdefghijkl')
+  end
+  
+  it 'should generate uppercase letters' do
+    allow(passwordgenerator).to receive(:makeit).and_return('ABCDEFGHIJKL')
+    expect(passwordgenerator.makeit).to eq ('ABCDEFGHIJKL')
+  end
+  
+  it 'should generate numbers' do
+    allow(passwordgenerator).to receive(:makeit).and_return('0123456789')
+    expect(passwordgenerator.makeit).to eq ('0123456789')
+  end
+  
+  it 'should have a default of 15 characters' do
+    expect(passwordgenerator.makeit.length).to eq 15
+  end
 end
