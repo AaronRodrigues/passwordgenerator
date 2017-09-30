1e0247aec523d4d96e5747fabcb818332ec5da1f
AaronRodrigues <aaronrod@gmail.com>
2017-09-30 23:11:39 +0100

    Refactors PasswordGenerator class -Creates seperate methods for creating arrays for each user story -Each method has a single responsibility -Makes create array methods private

--- lib/passwordgenerator.rb
+++ lib/passwordgenerator.rb
@@ -2,14 +2,29 @@ class PasswordGenerator
     
   def makeit(length = 15, uppercase = true, numeric = true, special = true)
     secret = []
-    lowercase_array = ('a'..'z').to_a
-    uppercase_array = ('A'..'Z').to_a
-    num_array  = (0..9).to_a
-    special_array = ("!$%&*@^").chars
-    secret.concat(lowercase_array.shuffle)
-    secret.concat(uppercase_array.shuffle) if uppercase == true
-    secret.concat(num_array.shuffle) if numeric == true
-    secret.concat(special_array.shuffle) if special == true
+    secret.concat(create_lowercase_array.shuffle)
+    secret.concat(create_uppercase_array.shuffle) if uppercase == true
+    secret.concat(create_num_array.shuffle) if numeric == true
+    secret.concat(create_special_array.shuffle) if special == true
     password = secret.shuffle.take(length).join
   end
+  
+  private
+  
+  def create_lowercase_array
+    lowercase_array = ('a'..'z').to_a
+  end
+  
+  def create_uppercase_array
+    uppercase_array = ('A'..'Z').to_a
+  end
+  
+  def create_num_array
+    num_array  = (0..9).to_a
+  end
+  
+  def create_special_array
+    special_array = ("!$%&*@^").chars
+  end
+
 end
