93c1dff31813318ca4e10dcd3d3c72c9ad4cb6b3
AaronRodrigues <aaronrod@gmail.com>
2017-09-30 19:02:39 +0100

    Adds functionality to add/remove uppercase letters

--- lib/passwordgenerator.rb
+++ lib/passwordgenerator.rb
@@ -1,9 +1,11 @@
 class PasswordGenerator
     
-  def makeit(length = 15)
+  def makeit(length = 15, uppercase = true)
     secret = []
     lowercase_array = ('a'..'z').to_a
+    uppercase_array = ('A'..'Z').to_a
     secret.concat(lowercase_array.shuffle)
+    secret.concat(uppercase_array.shuffle) if uppercase == true
     password = secret.shuffle.take(length).join
   end
 end
